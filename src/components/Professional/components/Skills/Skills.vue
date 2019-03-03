248948f49132e1536712efad0da272d6183fa574
Felix Morau <felixmorau@gmail.com>
2019-03-03 12:43:14 +0100

    initial commit

---
+++ src/components/Professional/components/Skills/Skills.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <skills-terminals />
+        <skills-u-x />
+    </div>    
+</template>
+
+<script>
+import SkillsTerminals from "./SkillsTerminals"
+import SkillsUX from "./SkillsUX"
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        SkillsTerminals,
+        SkillsUX
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+h3, h2 {
+    text-align: center;
+}
+
+
+
+</style>
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import ProfessionalContainer from "./components/Professional/ProfessionalContainer"
+
+
+export const routes = [
+    { path: "/", component: ProfessionalContainer },
+    { path: "/professional", component: ProfessionalContainer }
+];
